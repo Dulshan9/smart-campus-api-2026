9dfa6bfe9ddf6a13f867e8c43222ee13bed5f351
Dulshan9 <praveensankalpadulshan0909@gmail.com>
2026-04-13 19:25:59 +0530

    updating file structure

--- src/main/java/uk/ac/westminster/SmartCampusApplication.java
+++
@@ -1,8 +0,0 @@
-package uk.ac.westminster;
-
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
-
-@ApplicationPath("/api/v1")
-public class SmartCampusApplication extends Application {
-}
